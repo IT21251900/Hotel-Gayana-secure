90ede6111ea7f9faca230d65c91e699d76719c10
Sanjana Dinith <100860580+IT21251900@users.noreply.github.com>
2024-09-17 14:01:09 +0530

    Revert "clean up"
    This reverts commit 5ee5dfac0c12b020c97290efa268099955ecd974.

---
+++ backend/Server.js
@@ -0,0 +1,53 @@
+
+const express = require("express")
+const session = require("express-session");
+const mongoose = require("mongoose")
+require("dotenv").config()
+const config = require("./config/default.json")
+const routes = require("./routes/Route")
+const cors = require("cors")
+
+const postRoutesmenu = require('./routes/menu');
+const passport = require("passport");
+const authRoute = require("./routes/facebook.route")
+const app = express()
+const PORT = config.server.port || 8000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
+app.use(express.json())
+app.use(session({
+    secret: '+XqJcU38f7J1m7MBV8Fq2W5Q5e6uF4PqzUpZt9A9KCA=', 
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false } 
+  }));
+
+app.use(cors())
+app.use(passport.initialize());
+app.use(passport.session());
+app.use("/auth", authRoute);
+
+
+// MongoDB connection
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL || config.db.path, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error('MongoDB Connection Error:', err);
+    process.exit(1); 
+  }
+})();
+
+
+app.use("/api", routes)
+app.use(postRoutesmenu);
+
+app.listen(PORT, () => console.log("Listening at " + PORT));
+module.exports = app;
